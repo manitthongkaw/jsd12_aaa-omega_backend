b3335dc32a7ffc4e2762ecc8e470c97b728c07d0
Manit Thongkaw <manitthongkaw@gmail.com>
2026-06-08 08:59:06 +0700

    fix: rateLimit for dev and prod

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,8 +1,10 @@
 import { rateLimit } from "express-rate-limit";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100,
+	limit: isProd ? process.env.RATELIMIT : 1000,
 	standardHeaders: "draft-8",
 	legacyHeaders: false,
 	ipv6Subnet: 56
